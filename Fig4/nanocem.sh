8624f0b19548dd14031e4663f20c291ddef9e147
GuoZhihao <76717431+JeremyQuo@users.noreply.github.com>
2024-07-03 16:21:08 +0800

    Delete Fig4 directory

--- Fig4/nanocem.sh
+++
@@ -1,6 +0,0 @@
-$element is the position such as 5113
-# r9 flow cell
-current_events_magnifier f5c_ev -i WT/file -c WGA/file --chrom NC_005773.3 --strand + --pos $element --pore r9 --len 10 --ref ../1448A.fasta -o nanoCEM_result_f5c_ev_$element --norm -t 64
-
-# r10 flow cell
-current_events_magnifier f5c_ev -i WT/file -c WGA/file --chrom NC_005773.3 --strand + --pos $element --pore r10 --len 10 --ref ../1448A.fasta -o nanoCEM_result_f5c_ev_$element --norm -t 64
